3f728fb9bb5f8f6b43d8a3138df1c66030a34e77
KEstefania <155700190+KEstefania@users.noreply.github.com>
2024-01-11 22:16:27 -0500

    inicial commit

---
+++ js/01.js
@@ -0,0 +1,6 @@
+let nombre="Karla";
+let apellido="barrera";
+const cedula="1000100010";
+let direccion="Quinche";
+let fechadenacimiento="04 de noviembre del 2002";
+document.write(nombre+""+apellido+""+cedula);
